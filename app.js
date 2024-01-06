1aaf7efb3a6c00316e3ee84baee34182ddf7fb12
AKSHAY ANAND <akshay@cerebry.co>
2024-01-06 17:44:44 +0530

    api work done

--- app.js
+++ app.js
@@ -4,17 +4,23 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
 const userRouter = require('./routes/user');
+const noteRouter = require('./routes/note');
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
 app.get('/', (req, res)=>{
     res.status(200).send('<h3>Hello from backend system</h3>');
 });
 
 app.use(userRouter);
+app.use(noteRouter);
 
 module.exports = app;
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,26 +2,25 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
 const auth = async(req, res, next)=>{
-    console.log(`Req.cookies: ${req.cookies}`);
-
-    const token = req.cookies.token||req.body.token||req.header('Authorization').replace('Bearer ',"");
-    // const token = req.header('Authorization').replace('Bearer ',"");
-    if(!token){
-        return res.status(403).send('Token is missing');
-    }
-    console.log(`Token: ${token}`);
     try {
-        const decode =await jwt.verify(token, process.env.SECRET_KEY);
-        console.log(`decode: ${decode}`);
+        const token = req.cookies && req.cookies.token||req.body.token||req.header('Authorization')&& req.header('Authorization').replace('Bearer ',"");
+        // const token = req.header('Authorization').replace('Bearer ',"");
+        if(!token){
+            throw new Error(`Login token is missing`);
+        }
+
+        const decode = await jwt.verify(token, process.env.SECRET_KEY);
         if(decode){
             req.user = decode;
             next(); 
         }else
-            res.status(400).json({
-                error:"token verification authentication failed"
-            });
+            throw new Error(`Log in token authentication failed`);
     } catch (error) {
-        return res.status(400).json({error});
+        res.status(400).json({
+            success: false,
+            message: "error occurred in login token authentication",
+            error
+        });
     }
 }
 
